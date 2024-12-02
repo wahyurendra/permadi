aec7ec94b207a61cf274e0a3329b45ee99ad6cb7
wahyurendra <wahyurendraaditya@gmail.com>
2024-12-02 23:44:42 +0700

    update FE

--- FE-permadi/vite.config.mjs
+++ FE-permadi/vite.config.mjs
@@ -1,9 +1,9 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
-import { PrimeVueResolver } from '@primevue/auto-import-resolver';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,5 +17,10 @@ export default defineConfig({
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
+    },
+    server: {
+        host: '0.0.0.0', // Allows access from other devices on the network
+        port: 3000,      // The port you want to use (default is 5173)
+        strictPort: true // Ensures Vite doesn't switch to a different port if 3000 is taken
     }
 });
