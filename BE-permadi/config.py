186eaa9d60d5b762d762b5460d430649e4d80c53
wahyurendra <wahyurendraaditya@gmail.com>
2024-12-03 00:36:06 +0700

    update BE

--- BE-permadi/config.py
+++ BE-permadi/config.py
@@ -3,6 +3,6 @@ import os
 DATABASE_USER = os.environ.get("DATABASE_USER", "root")
 DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD", "root")
 DATABASE_HOST = os.environ.get("DATABASE_HOST", "127.0.0.1:27017")
-DATABASE_URI = f'mongodb://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/'
+DATABASE_URI = f'mongodb+srv://muhammadjalaludin548:jAqGb0oc2VBZ0NOs@permadi.9xvaf.mongodb.net/?'
 
 SECRET = os.getenv("SECRET", "Permadi")
